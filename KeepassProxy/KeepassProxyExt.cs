21d23f91bd17411fc65258f56d9891fe80e244ad
skyoflw <skyoflw@gmail.com>
2017-08-05 13:17:55 +0800

    init proj.

---
+++ KeepassProxy/KeepassProxyExt.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using KeePass.Plugins;
+
+namespace KeepassProxy
+{
+    public class KeepassProxyExt : Plugin
+    {
+        private const string KeyProxy = "DF3BB3C8-F3A4-4AFF-A43A-68381DFED0CF";
+
+        private string _proxy;
+        private IPluginHost _host;
+
+        public override bool Initialize(IPluginHost host)
+        {
+            this._host = host;
+            this._proxy = host.CustomConfig.GetString(KeyProxy, null);
+            this.SetProxy(this._proxy);
+
+            var items = host.MainWindow.ToolsMenu.DropDownItems;
+            var menu = new ToolStripMenuItem("Keepass Proxy Options");
+            menu.Click += this.MenuOnClick;
+            items.Add(menu);
+
+            return true;
+        }
+
+        private void SetProxy(string proxy)
+        {
+            WebRequest.DefaultWebProxy = !string.IsNullOrWhiteSpace(proxy) ? new WebProxy(proxy) : null;
+        }
+
+        private void MenuOnClick(object sender, EventArgs eventArgs)
+        {
+            var form = new ProxySettingsForm(this._proxy);
+            if (form.ShowDialog() == DialogResult.OK)
+            {
+                this._proxy = form.HostResult;
+                this.SetProxy(this._proxy);
+                this._host.CustomConfig.SetString(KeyProxy, this._proxy);
+            }
+        }
+    }
+}
---
+++ KeepassProxy/ProxySettingsForm.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KeepassProxy
+{
+    public partial class ProxySettingsForm : Form
+    {
+        public ProxySettingsForm()
+        {
+            this.InitializeComponent();
+        }
+
+        public ProxySettingsForm(string host)
+            : this()
+        {
+            this.HostText.Text = host ?? string.Empty;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var host = this.HostText.Text.Trim();
+            if (string.IsNullOrWhiteSpace(host))
+            {
+                this.HostResult = string.Empty;
+            }
+            else
+            {
+                var match = Regex.Match(host, "^(.+)\\:(\\d{1,5})$");
+                if (!match.Success)
+                {
+                    MessageBox.Show("Invaild Proxy", "Error");
+                    return;
+                }
+                var port = int.Parse(match.Groups[2].Value);
+                if (port < 1 || port >= 65535)
+                {
+                    MessageBox.Show("Invaild Port", "Error");
+                    return;
+                }
+
+                this.HostResult = host;
+            }
+
+            this.DialogResult = DialogResult.OK;
+        }
+
+        public string HostResult { get; private set; }
+    }
+}
